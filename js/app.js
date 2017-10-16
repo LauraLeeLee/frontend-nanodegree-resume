ffe3185881a01b4ea7ca19870c3c288ddb1b6439
Laura Logan <laurardh@aol.com>
2017-10-16 16:02:53 -0400

    adds the init functions of the views to the MOV restructure

--- js/app.js
+++ js/app.js
@@ -131,5 +131,46 @@ var bioView = {
 
     var HTMLbioPic = '<img src="%data%" class="biopic">';
     var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
+
+    var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-column"></ul>';
+    var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
+  }
+};
+
+var workView = {
+  init: function(){
+    var HTMLworkStart = '<div class="work-entry"></div>';
+    var HTMLworkEmployer = '<a href="#">%data%';
+    var HTMLworkTitle = ' - %data%</a>';
+    var HTMLworkDates = '<div class="date-text">%data%</div>';
+    var HTMLworkLocation = '<div class="location-text">%data%</div>';
+    var HTMLworkDescription = '<p><br>%data%</p>';
+  }
+};
+
+var projectView = {
+  init: function(){
+    var HTMLprojectStart = '<div class="project-entry blue"></div>';
+    var HTMLprojectTitle = '<a href="#">%data%</a>';
+    var HTMLprojectDates = '<div class="date-text">%data%</div>';
+    var HTMLprojectDescription = '<p><br>%data%</p>';
+    var HTMLprojectImage = '<img src="%data%" id="%id">';
+  }
+};
+
+var educationView = {
+  init: function(){
+    var HTMLschoolStart = '<div class="education-entry"></div>';
+    var HTMLschoolName = '<a href="#">%data%';
+    var HTMLschoolDegree = ' -- %data%</a>';
+    var HTMLschoolDates = '<div class="date-text">%data%</div>';
+    var HTMLschoolLocation = '<div class="location-text">%data%</div>';
+    var HTMLschoolMajor = '<em><br>Major: %data%</em>';
+
+    var HTMLonlineClasses = '<h3>Online Classes</h3>';
+    var HTMLonlineTitle = '<a href="#">%data%';
+    var HTMLonlineSchool = ' - %data%</a>';
+    var HTMLonlineDates = '<div class="date-text">%data%</div>';
+    var HTMLonlineURL = '<br><a href="#">%data%</a>';  
   }
 };
